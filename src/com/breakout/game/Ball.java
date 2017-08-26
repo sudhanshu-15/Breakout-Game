cc9aa34d1f51c944e66a9cc1c0be7b39a647360d
Sudhanshu Siddh <sudhanshu.15@gmail.com>
2017-08-26 02:49:16 -0400

    Added position and velocity for ball class with respective getters/setters

--- src/com/breakout/game/Ball.java
+++ src/com/breakout/game/Ball.java
@@ -1,5 +1,60 @@
 package com.breakout.game;
 
-public class Ball {
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Ball implements GameElementInterface{
 	
+	//Ball class for the game
+	
+	private int posX; 	//X-coordinate position of ball
+	private int posY;	//Y-coordinate position of ball
+	private int velX;	//Velocity on X-axis
+	private int velY;	//Velocity on Y-axis
+	
+	public Ball(int posX, int posY){
+		this.posX = posX;
+		this.posY = posY;
+		
+		velX = 0;
+		velY = 0;
+		
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public int getVelX() {
+		return velX;
+	}
+
+	public void setVelX(int velX) {
+		this.velX = velX;
+	}
+
+	public int getVelY() {
+		return velY;
+	}
+
+	public void setVelY(int velY) {
+		this.velY = velY;
+	}
+	
+	public void draw(Graphics g){
+		g.setColor(Color.ORANGE);				//TO-DO: Add variable for color
+		g.fillOval(posX, posY, 20, 20); 		//TO-DO: Add import for constants and add dimensions
+	}
 }
