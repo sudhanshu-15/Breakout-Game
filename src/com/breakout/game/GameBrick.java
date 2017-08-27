7b0774a40b4458a87078b73b37f438f3fe00478e
anchal <ankhande@iu.edu>
2017-08-27 11:21:04 -0400

    brick file added

---
+++ src/com/breakout/game/GameBrick.java
@@ -0,0 +1,42 @@
+package com.breakout.game;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class GameBrick{
+	public int brickArray[][];
+	public int brickHeight;
+	public int brickWidth;
+	
+	public GameBrick(int rows, int columns){
+		brickHeight = 40;
+		brickWidth = 80;
+		brickArray = new int[rows][columns];
+		
+		for(int i = 0; i<brickArray.length;++i){
+			for(int j = 0;j<brickArray[0].length;++j){
+				brickArray[i][j] = 1;
+			}
+		}
+	}
+	
+	public void draw(Graphics2D g){
+		for(int i =0; i<brickArray.length;++i){
+			for(int j = 0; j<brickArray[0].length;++j){
+				if(brickArray[i][j]>0){
+					g.setColor(Color.BLACK);
+					g.fillRect(j*brickWidth+10, i*brickHeight+40, brickWidth, brickHeight);
+					
+					g.setStroke(new BasicStroke(3));
+					g.setColor(Color.WHITE);
+					g.drawRect(j*brickWidth+10, i*brickHeight+40, brickWidth, brickHeight);
+				}
+			}
+		}
+	}
+	
+	public void setBrickValue(int value, int row, int column){
+		brickArray[row][column] = value;
+	}
+}
