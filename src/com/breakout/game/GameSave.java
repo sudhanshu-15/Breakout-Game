0437f86eb82c9487796a8723f41e30778dc78c51
Pulkit Mathur <pulmath@iu.edu>
2017-09-16 18:56:23 -0400

    Allowing User to decide file save location - F2017A4T4-4 - http://tintin.cs.indiana.edu:8091/browse/F2017A4T4-4

--- src/com/breakout/game/GameSave.java
+++ src/com/breakout/game/GameSave.java
@@ -1,10 +1,14 @@
 package com.breakout.game;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
 
 import com.breakout.command.MacroCommand;
 
@@ -21,16 +25,28 @@ public class GameSave {
 	public boolean Serialize() 
 	{
 		try {
-	         FileOutputStream fileOut = new FileOutputStream("C:\\Users\\mathu\\Desktop\\JSON\\gameData.txt");
-	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
-	         out.writeObject(list);
-	         out.close();
-	         fileOut.close();
-	         //System.out.printf("Serialized data is saved in /tmp/employee.ser");
-	         return true;
-	      }catch(Exception i) {
-	         i.printStackTrace();
-	      }
+
+			JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+			jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			jfc.setApproveButtonText("Save");
+			jfc.setApproveButtonMnemonic('s');
+			jfc.setApproveButtonToolTipText("Save Game");
+			int returnValue = jfc.showOpenDialog(null);
+			if (returnValue == JFileChooser.APPROVE_OPTION) {
+				File selectedFile = jfc.getSelectedFile();
+				FileOutputStream fileOut = new FileOutputStream(selectedFile.getPath()+".txt");
+				ObjectOutputStream out = new ObjectOutputStream(fileOut);
+				out.writeObject(list);
+				out.close();
+				fileOut.close();
+				return true;
+			}
+			else {
+				return false;
+			}
+		}catch(Exception i) {
+			i.printStackTrace();
+		}
 		return false;
 	}
 
