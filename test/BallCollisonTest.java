c85702cb160020f24ff348569074242a43a2a7a8
Nikilesh Chilkuru <nchilkur@iu.edu>
2017-09-17 20:17:06 -0400

    F2017A4T4-22 http://tintin.cs.indiana.edu:8091/projects/F2017A4T4/issues/F2017A4T4-22?filter=resolvedrecently - Merging the test cases into Load and Layout branch

--- test/BallCollisonTest.java
+++ test/BallCollisonTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.awt.Color;
 import java.awt.Rectangle;
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,21 +13,22 @@ import com.breakout.game.GameBall;
 import com.breakout.game.GameBrick;
 import com.breakout.game.GamePaddle;
 import com.breakout.command.BallCommand;
+import com.breakout.command.MacroCommand;
 
 public class BallCollisonTest {
 
+	Boolean dead = true;
+	GameBall ball = new GameBall(11,11,1,1, Color.BLACK);
+	GameBrick brick = new GameBrick(12,12);
+	BallCommand bc = new BallCommand(ball);
 	
-	GameBall ball;
 	@Before 
 	public void excutedbeforeeach() {
 	}
 
 	@Test
-	public void test() {
-		Boolean dead = true;
-		GameBall ball = new GameBall(11,11,1,1, Color.BLACK);
-		GameBrick brick = new GameBrick(12,12);
-		BallCommand bc = new BallCommand(ball);
+	public void testBallCollision() {
+
 		
 		Rectangle ballCollider = ball.createCollider(ball.getPosX(), ball.getPosY(), 20, 20);
 		
@@ -35,6 +38,32 @@ public class BallCollisonTest {
 		{ dead = false;}
 		
 		assertEquals(false, dead);
-
+	}
+	@Test
+	public void testGameBallgetPosX() {
+		int expectedOutput = 11;
+		int actualOutput = ball.getPosX();
+		assertEquals(expectedOutput, actualOutput);
+	}
 		
-	}}
+	@Test
+	public void testGameBallgetPosY() {
+		int expectedOutput = 11;
+		int actualOutput = ball.getPosY();
+		assertEquals(expectedOutput, actualOutput);
+		}
+	
+	@Test
+	public void testGameBallgetVelX() {
+		int expectedOutput = 1;
+		int actualOutput = ball.getVelX();
+		assertEquals(expectedOutput, actualOutput);
+		}
+	
+	@Test
+	public void testGameBallgetVelY() {
+		int expectedOutput = 1;
+		int actualOutput = ball.getVelY();
+		assertEquals(expectedOutput, actualOutput);
+		}
+}
